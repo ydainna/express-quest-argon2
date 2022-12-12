e308c2be4a90c09b3a1be0c1125fab8f7572f194
saigyou_ayakashi <minuit1993@live.fr>
2022-12-12 12:01:18 +0100

    update

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -11,8 +11,6 @@ const hashPassword = (req, res, next) => {
   argon2
     .hash(req.body.password, hashingOptions)
     .then((hashedPassword) => {
-      console.log(hashedPassword);
-
       req.body.hashedPassword = hashedPassword;
       delete req.body.password;
       next();
--- routes/userHandlers.js
+++ routes/userHandlers.js
@@ -2,7 +2,7 @@ const database = require("./../database/database");
 
 //GET all users
 const getUsers = (req, res) => {
-  const initialSql = "select * from users";
+  const initialSql = "select firstname, lastname, email, city, language from users";
   const where = [];
 
   if (req.query.language != null) {
@@ -43,7 +43,7 @@ const getUserById = (req, res) => {
   const id = parseInt(req.params.id);
 
   database
-    .query("select * from users where id = ?", [id])
+    .query("select firstname, lastname, email, city, language from users where id = ?", [id])
     .then(([users]) => {
       if (users[0] != null) {
         res.json(users[0]);
